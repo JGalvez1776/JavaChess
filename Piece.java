cab2aedbef5dd4f93f0e2b849ed967293da7d8b3
jgalvez1776 <jgalvez1776@gmail.com>
2020-12-19 14:20:55 -0700

    Added basic knight class

--- Piece.java
+++ Piece.java
@@ -5,6 +5,8 @@ public abstract class Piece {
     private String team;
     
 
+    /*TODO: Implement move function. Should be the same may implement in the board? */
+
     public abstract void findMoves(Board board);
     
 
