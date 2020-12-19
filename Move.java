062ed4e299eb9972e6cda5ee582e5783b6e43092
JGalvez1776 <64766167+JGalvez1776@users.noreply.github.com>
2020-12-18 21:51:13 -0700

    Add files via upload

---
+++ Move.java
@@ -0,0 +1,20 @@
+public class Move {
+    int x;
+    int y;
+
+
+    public Move() {
+        this(0, 0);
+    }
+
+    public Move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "Test";
+    }
+
+
+}
