51acf9b26b18b3656d908fdac5d220daf680ae87
JGalvez1776 <jgalvez1776@gmail.com>
2020-12-23 21:11:15 -0700

    Added printBoard method

--- Game.java
+++ Game.java
@@ -14,12 +14,23 @@ public class Game {
         
     }
 
+    /**
+     * @return Next active player
+     */
     public Player nextPlayer() {
         activePlayerNum++;
         if (activePlayerNum > players.size() - 1) {
             activePlayerNum = 0;
         }
         return players.get(activePlayerNum);
+    }
+
+    public void printBoard() {
+        System.out.println(board);
+    }
+
+    public String toString() {
+        return players.toString();
     }
 
 } 
